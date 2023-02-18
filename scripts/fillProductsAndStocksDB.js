f40fb5fb77d419e61fd6339f42044d940314eb37
AlinaFirsova <firsova.a@winsolutions.ru>
2023-02-19 00:36:37 +0300

    Module 4

---
+++ scripts/fillProductsAndStocksDB.js
@@ -0,0 +1,52 @@
+import { Client } from "pg";
+import products from "../products";
+
+const PRODUCTS_DB_HOST = process.env.PRODUCTS_DB_HOST;
+const PRODUCTS_DB_PORT = process.env.PRODUCTS_DB_PORT || 5432;
+const PRODUCTS_DB_USER = process.env.PRODUCTS_DB_USER;
+const PRODUCTS_DB_PASSWORD = process.env.PRODUCTS_DB_PASSWORD;
+
+const client = new Client({
+  host: PRODUCTS_DB_HOST,
+  port: PRODUCTS_DB_PORT,
+  user: PRODUCTS_DB_USER,
+  password: PRODUCTS_DB_PASSWORD,
+});
+
+(async () => {
+  try {
+    await client.connect();
+    console.log(`Successful connection to the DB ${PRODUCTS_DB_HOST}`);
+
+    for (const product of products) {
+      const { title, price, description, id, count } = product;
+
+      try {
+        console.log(`Starting transaction for product id ${id}...`);
+        await client.query("BEGIN");
+
+        const insertProductQuery =
+          "INSERT INTO products(id, title, description, price) VALUES($1, $2, $3, $4)";
+        await client.query(insertProductQuery, [id, title, description, price]);
+
+        const insertStockQuery =
+          "INSERT INTO stocks(product_id, count) VALUES($1, $2)";
+        await client.query(insertStockQuery, [id, count]);
+
+        await client.query("COMMIT");
+        console.log(`Product with id ${id} was successfully added`);
+      } catch (e) {
+        await client.query("ROLLBACK");
+        console.error(`ERROR: Product with id ${id} wasn't added`, e.message);
+      }
+    }
+  } catch (e) {
+    console.error(
+      `ERROR: Can't connect to the DB ${PRODUCTS_DB_HOST}`,
+      e.stack
+    );
+  } finally {
+    client.end();
+    console.log(`Connection to the DB ${PRODUCTS_DB_HOST} was closed`);
+  }
+})();
--- src/handlers/getProductById.js
+++ src/handlers/getProductById.js
@@ -1,24 +1,54 @@
-import products from "../../products";
+import { Client } from "pg";
 import { headers } from "../../constants";
 
-export const main = async (event) => {
-    try {
-      const { productId } = event.pathParameters;
-      const product = products.find(({ id }) => id === productId);
-      const statusCode = product ? 200 : 404;
-      const notFoundMessage = `Error: Can't fing product with id ${productId}`;
-      const body = product ? JSON.stringify(product) : notFoundMessage;
+const PRODUCTS_DB_HOST = process.env.PRODUCTS_DB_HOST;
+const PRODUCTS_DB_PORT = process.env.PRODUCTS_DB_PORT || 5432;
+const PRODUCTS_DB_USER = process.env.PRODUCTS_DB_USER;
+const PRODUCTS_DB_PASSWORD = process.env.PRODUCTS_DB_PASSWORD;
 
-      return {
-        statusCode,
-        headers,
-        body,
-      };
-    } catch(e) {
-      return {
-        statusCode: 500,
-        headers,
-        body: `The server couldn't process this request: ${e}`,
-      };
-    }
+export const main = async (event) => {
+  const client = new Client({
+    host: PRODUCTS_DB_HOST,
+    port: PRODUCTS_DB_PORT,
+    user: PRODUCTS_DB_USER,
+    password: PRODUCTS_DB_PASSWORD,
+  });
+
+  try {
+    const { productId } = event.pathParameters;
+    await client.connect();
+    console.log(`Successful connection to the DB ${PRODUCTS_DB_HOST}`);
+    console.log(`Fetching product with id ${productId}...`);
+
+    const getProductQuery =
+      "SELECT * FROM products INNER JOIN stocks ON products.id = stocks.product_id WHERE products.id=$1";
+    const { rows: products } = await client.query(getProductQuery, [productId]);
+    const product = products.pop();
+
+    const statusCode = product ? 200 : 404;
+    const notFoundMessage = `Error: Can't fing product with id ${productId}`;
+    const body = product ? JSON.stringify(product) : notFoundMessage;
+
+    console.log(`Product with id ${productId} was successfully fetched: ${product}`);
+    return {
+      statusCode,
+      headers,
+      body,
+    };
+  } catch (e) {
+    const errorMessage = e.message || e;
+    console.error(
+      "ERROR: Can't fetch the product:",
+      errorMessage
+    );
+
+    return {
+      statusCode: 500,
+      headers,
+      body: `The server couldn't process this request: ${errorMessage}`,
+    };
+  } finally {
+    client.end();
+    console.log(`Connection to the DB ${PRODUCTS_DB_HOST} was closed`);
+  }
 };
